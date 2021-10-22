d032e354ca18a65e2525296fab2bd6f26b2a4390
ConstantineTU <8kozmo8@gmail.com>
2021-10-23 02:05:59 +0500

    feat: add time and date

--- momentum/js/app.js
+++ momentum/js/app.js
@@ -1,5 +1,33 @@
 
+// ? Часы и время 
 
+function showTime() {
+	const time = document.querySelector('.time')
+	const dateLocal = new Date()
+	const currentTime = dateLocal.toLocaleTimeString()
+	time.textContent = currentTime
+	showDate();
+	setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+	const date = document.querySelector('.date')
+	const dateLocal = new Date()
+	const options = {
+
+
+		month: 'long',
+		day: 'numeric',
+		weekday: 'long',
+
+	};
+	// TODO Добавить смену языка
+	const currentDate = dateLocal.toLocaleDateString('ru-RU', options);
+	date.textContent = currentDate
+	setTimeout(showDate, 1000);
+}
+// End Time and Date
 
 
 
