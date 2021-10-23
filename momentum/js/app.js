b8b1768c94d17a33cfd74ae4a199d34d6f9d5314
ConstantineTU <8kozmo8@gmail.com>
2021-10-23 15:59:55 +0500

    feat: add slider bg images

--- momentum/js/app.js
+++ momentum/js/app.js
@@ -8,8 +8,7 @@ function showTime() {
 	time.textContent = currentTime
 	showDate()
 	showGreeting()
-
-	setTimeout(showTime, 10);
+	setTimeout(showTime, 990);
 }
 showTime();
 
@@ -28,7 +27,7 @@ function getTimeOfDay() {
 	const hours = date.getHours()
 	if (hours / 6 < 1) return 'night'
 	else if (hours / 6 < 2) return 'morning'
-	else if (hours / 6 < 3) return 'day'
+	else if (hours / 6 < 3) return 'afternoon'
 	else return 'evening'
 }
 getTimeOfDay()
@@ -40,15 +39,14 @@ function showGreeting() {
 	greeting.textContent = greetingText
 }
 
+// Save
 function setLocalStorage() {
 	const name = document.querySelector('.name')
 	localStorage.setItem('name', name.value)
 }
 window.addEventListener('beforeunload', setLocalStorage)
 
-
-
-
+// Load
 function getLocalStorage() {
 	const name = document.querySelector('.name')
 	if (localStorage.getItem('name') && localStorage.getItem('name') !== 'undefined') {
@@ -56,6 +54,67 @@ function getLocalStorage() {
 	}
 }
 window.addEventListener('load', getLocalStorage)
+
+// ? Slider bg
+
+let randomNum
+let loading = true
+
+function bgNum(mi, ma) {
+	let min = Math.ceil(mi);
+	let max = Math.floor(ma);
+	const result = String(Math.floor(Math.random() * (max - min + 1)) + min)
+	randomNum = result.padStart(2, '0')
+}
+
+
+bgNum(1, 20)
+
+
+
+function setBg() {
+	const body = document.querySelector('body')
+	const img = new Image();
+	loading = false
+	img.src = `https://raw.githubusercontent.com/ConstantineTU/stage1-tasks/assets/images/${getTimeOfDay()}/${randomNum}.jpg`
+	img.onload = () => {
+
+		body.style.backgroundImage =
+			`url('https://raw.githubusercontent.com/ConstantineTU/stage1-tasks/assets/images/${getTimeOfDay()}/${randomNum}.jpg')`
+		loading = true
+	}
+}
+
+
+setBg()
+
+
+document.querySelector('.slide-prev').addEventListener('click', getSlidePrev)
+document.querySelector('.slide-next').addEventListener('click', getSlideNext)
+
+function getSlideNext() {
+	if (loading) {
+		if (randomNum == '20') {
+			randomNum = 1
+		} else {
+			randomNum++
+		}
+		randomNum = String(randomNum).padStart(2, '0')
+		setBg()
+	}
+}
+
+function getSlidePrev() {
+	if (loading) {
+		if (randomNum == '01') {
+			randomNum = 20
+		} else {
+			randomNum--
+		}
+		randomNum = String(randomNum).padStart(2, '0')
+		setBg()
+	}
+}
 
 
 
